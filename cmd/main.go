c28b7f30005e2a7d5cb3962399cb41f49691ae8b
LukasLimalkl <lukinhaslima063@gmail.com>
2024-04-18 11:39:56 -0300

    feat: starting connectio to sheets

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/LukasLimalkl/api-sheets/pkg/notion"
+	"github.com/LukasLimalkl/api-sheets/pkg/sheets"
 )
 
 func main() {
-	notion.ConnectNotion()
+	sheets.ConnectSheets()
 }
--- pkg/sheets/sheets.go
+++ pkg/sheets/sheets.go
@@ -3,14 +3,23 @@ package sheets
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
 func ConnectSheets() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	apiKey := os.Getenv("API_SHEETS")
 	ctx := context.Background()
-	sheetsService, err := sheets.NewService(ctx, option.WithAPIKey("client"))
+	sheetsService, err := sheets.NewService(ctx, option.WithAPIKey(apiKey))
 	if err != nil {
 		fmt.Println(err)
 	}
