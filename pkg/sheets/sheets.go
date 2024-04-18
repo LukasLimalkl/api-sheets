b307419e3462cd85989d4e935294eddfbf82e20a
LukasLimalkl <lukinhaslima063@gmail.com>
2024-04-18 17:40:14 -0300

    feat: sheets db

--- pkg/sheets/sheets.go
+++ pkg/sheets/sheets.go
@@ -2,11 +2,13 @@ package sheets
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
@@ -17,11 +19,41 @@ func ConnectSheets() {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
 
-	apiKey := os.Getenv("API_SHEETS")
 	ctx := context.Background()
-	sheetsService, err := sheets.NewService(ctx, option.WithAPIKey(apiKey))
+
+	config := &oauth2.Config{}
+	token, err := config.Exchange(ctx, ...)
+	
+	sheetKey := os.Getenv("KEY_SHEETS")
+	sheetsService, err := sheets.NewService(ctx, option.WithTokenSource())
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(sheetsService)
+
+	data, err := os.ReadFile("./dados.json")
+	if err != nil {
+		log.Fatalf("Error reading JSON file: %s", err)
+	}
+
+	var records []map[string]interface{}
+	if err := json.Unmarshal(data, &records); err != nil {
+		log.Fatalf("Error decoding JSON: %s", err)
+	}
+
+	var vr sheets.ValueRange
+	for _, record := range records {
+		var row []interface{}
+		for _, value := range record {
+			row = append(row, value)
+		}
+		vr.Values = append(vr.Values, row)
+	}
+
+	dataRange := "A1"
+
+	_, err = sheetsService.Spreadsheets.Values.Append(sheetKey, dataRange, &vr).ValueInputOption("USER_ENTERED").Do()
+	if err != nil {
+		log.Fatalf("Unable to write data to sheet: %v", err)
+	}
+
 }
