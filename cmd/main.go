546fba958a611f1ff321a83bba10f0ffdb72f8c5
LukasLimalkl <lukinhaslima063@gmail.com>
2024-04-24 20:25:45 -0300

    feat: starting assas api

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"github.com/LukasLimalkl/api-sheets/pkg/notion"
 	"github.com/LukasLimalkl/api-sheets/pkg/sheets"
 )
 
 func main() {
-	notion.ConnectNotion()
 	sheets.ConnectSheets()
 
 }
