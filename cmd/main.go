beba80d16a73526969c189a0f0244fb51501925c
LukasLimalkl <lukinhaslima063@gmail.com>
2024-04-27 21:07:26 -0300

    feat: nada

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/LukasLimalkl/api-sheets/pkg/assas"
 	"github.com/LukasLimalkl/api-sheets/pkg/sheets"
 )
 
 func main() {
+	assas.Assas()
 	sheets.ConnectSheets()
 
 }
